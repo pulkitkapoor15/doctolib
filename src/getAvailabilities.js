c56fae91c16219786da4bae7fb9707ff67450806
Pulkit Kapoor <pulkitkapoor15@gmail.com>
2019-08-30 17:23:58 +0530

    Update the logic to get correct availabilities

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -3,6 +3,7 @@ import knex from "knexClient";
 
 export default async function getAvailabilities(date) {
   const availabilities = new Map();
+  let finalAvailableSlots = [];
   for (let i = 0; i < 7; ++i) {
     const tmpDate = moment(date).add(i, "days");
     availabilities.set(tmpDate.format("d"), {
@@ -17,23 +18,35 @@ export default async function getAvailabilities(date) {
     .where(function() {
       this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
     });
-
+  let openings = [];
+  let busySlots = [];
   for (const event of events) {
-    for (
-      let date = moment(event.starts_at);
-      date.isBefore(event.ends_at);
-      date.add(30, "minutes")
-    ) {
-      const day = availabilities.get(date.format("d"));
-      if (event.kind === "opening") {
-        day.slots.push(date.format("H:mm"));
-      } else if (event.kind === "appointment") {
-        day.slots = day.slots.filter(
-          slot => slot.indexOf(date.format("H:mm")) === -1
-        );
-      }
+    if (event.kind == "opening") {
+      openings.push(event);
     }
   }
 
-  return Array.from(availabilities.values())
+  for (const event of events) {
+    if (event.kind != "opening") {
+      busySlots.push(event);
+    }
+  }
+  for (const opening of openings) {
+    for ( let date = moment(opening.starts_at); date.isBefore(opening.ends_at); date.add(30, "minutes")) {
+      const getAvailabilityForADay = availabilities.get(date.format("d"));
+      if (opening.starts_at - getAvailabilityForADay.date.getTime() < 86400000)
+        getAvailabilityForADay.slots.push(date.format("H:mm"));
+    }
+  }
+  for (const busySlot of busySlots) {
+    for ( let date = moment(busySlot.starts_at); date.isBefore(busySlot.ends_at); date.add(30, "minutes")) {
+      const getAvailabilityForADay = availabilities.get(date.format("d"));
+      getAvailabilityForADay.slots = getAvailabilityForADay.slots.filter(
+        slot => slot.indexOf(date.format("H:mm")) === -1
+      );
+    }
+  }
+  finalAvailableSlots = finalAvailableSlots.concat(Array.from(availabilities.values()));
+
+  return finalAvailableSlots;
 }
